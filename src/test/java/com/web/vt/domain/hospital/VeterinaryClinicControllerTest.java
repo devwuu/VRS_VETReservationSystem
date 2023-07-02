4da3990e6761edb890e3d50f2a8ccfcb5b9809e1
devwuu <jdw0477@gmail.com>
2023-07-02 15:03:05 +0900

    add spring rest docs configure

--- src/test/java/com/web/vt/domain/hospital/VeterinaryClinicControllerTest.java
+++ src/test/java/com/web/vt/domain/hospital/VeterinaryClinicControllerTest.java
@@ -3,18 +3,21 @@ package com.web.vt.domain.hospital;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.restdocs.AutoConfigureRestDocs;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
-import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
+import static org.springframework.restdocs.operation.preprocess.Preprocessors.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 
 @SpringBootTest
 @AutoConfigureMockMvc
+@AutoConfigureRestDocs
 class VeterinaryClinicControllerTest {
 
     @Autowired
@@ -34,7 +37,9 @@ class VeterinaryClinicControllerTest {
         mvc.perform(MockMvcRequestBuilders.post("/v1/rest/clinic/save")
                 .content(mapper.writeValueAsString(vo))
                 .contentType(MediaType.APPLICATION_JSON))
-                .andDo(print())
+                .andDo(document("clinic/save",
+                        preprocessRequest(prettyPrint()),
+                        preprocessResponse(prettyPrint())))
                 .andExpect(status().isCreated());
 
     }
