fdc5b270b999e6ccca7c01a146fdd78fce5bcaef
devwuu <jdw0477@gmail.com>
2023-07-18 10:37:09 +0900

    apply default value for gender

--- src/main/java/com/web/vt/domain/common/enums/GenderConverter.java
+++ src/main/java/com/web/vt/domain/common/enums/GenderConverter.java
@@ -1,5 +1,6 @@
 package com.web.vt.domain.common.enums;
 
+import com.web.vt.utils.ObjectUtil;
 import jakarta.persistence.AttributeConverter;
 
 import java.util.EnumSet;
@@ -8,7 +9,7 @@ import java.util.NoSuchElementException;
 public class GenderConverter implements AttributeConverter<Gender, String> {
     @Override
     public String convertToDatabaseColumn(Gender attribute) {
-        return attribute.code();
+        return ObjectUtil.isNotEmpty(attribute) ? attribute.code() : Gender.ETC.code();
     }
 
     @Override
